405fb503a0f84856ab5fe250c2543989d5f80b8a
JOAO VICTOR RODRIGUES COIMBRA <CG3038378@car.ifsp>
2026-03-26 17:38:19 -0300

    omnirolex

--- logicamente/src/app/layout.tsx
+++ logicamente/src/app/layout.tsx
@@ -1,48 +1,48 @@
-import type { Metadata } from "next";
-import { Audiowide, Golos_Text } from "next/font/google";
-import localFont from "next/font/local";
-import "./globals.css";
-import path from "path";
-import NavBar from "@/Components/NavBar/NavBar";
+import type { Metadata } from 'next';
+import { Audiowide, Golos_Text } from 'next/font/google';
+import localFont from 'next/font/local';
+import './globals.css';
+import NavBar from '@/components/NavBar/NavBar';
 
 const audiowide = Audiowide({
-  weight: "400",
-  variable: "--font-audiowide",
-  subsets: ["latin"],
+  weight: '400',
+  variable: '--font-audiowide',
+  subsets: ['latin'],
 });
 
 const golostext = Golos_Text({
-  weight: "400",
-  variable: "--font-golos-text",
-  subsets: ["latin"],
+  weight: '400',
+  variable: '--font-golos-text',
+  subsets: ['latin'],
 });
-
-export const metadata: Metadata = {
-  title: "Logicamente",
-  description: "Loja de Logica",
-};
 
 const lufga = localFont({
   src: [
     {
-      path: "../../public/Lufga/Fontspring-DEMO-lufga-regular.otf",
-      weight: "400",
-      style: "normal",
+      path: '../../public/Lufga/Fontspring-DEMO-lufga-regular.otf',
+      weight: '400',
+      style: 'normal',
     },
 
     {
-      path: "../../public/Lufga/Fontspring-DEMO-lufga-medium.otf",
-      weight: "500",
-      style: "normal",
+      path: '../../public/Lufga/Fontspring-DEMO-lufga-medium.otf',
+      weight: '500',
+      style: 'normal',
     },
+
     {
-      path: "../../public/Lufga/Fontspring-DEMO-lufga-bold.otf",
-      weight: "700",
-      style: "normal",
-    }
+      path: '../../public/Lufga/Fontspring-DEMO-lufga-bold.otf',
+      weight: '700',
+      style: 'normal',
+    },
   ],
-  variable: "--font-lufga",
+  variable: '--font-lufga',
 });
+
+export const metadata: Metadata = {
+  title: 'Marichique',
+  description: 'Loja de roupas femininas, masculinas e infantis',
+};
 
 export default function RootLayout({
   children,
@@ -50,7 +50,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br">
+    <html lang='en'>
       <body
         className={`${audiowide.variable} ${golostext.variable} ${lufga.variable}`}
       >
